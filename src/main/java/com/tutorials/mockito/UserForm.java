cd9d2b8bc955247a0b10db4949914899d9ed8f31
RachidSAOUDI <102986947+RachidSAOUDI@users.noreply.github.com>
2023-08-25 10:34:52 +0100

    Create UserForm.java

---
+++ src/main/java/com/tutorials/mockito/UserForm.java
@@ -0,0 +1,9 @@
+package com.tutorials.mockito;
+
+public class UserForm {
+    public String password;
+    public String username;
+    public String getUsername() {
+        return username;
+    }
+}
